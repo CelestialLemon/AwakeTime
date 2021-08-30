42c9f83a770f823e5efbb347e2b6b2ece6b244c5
Nightmare2202 <ashutoshinde23@gmail.com>
2021-08-30 10:01:48 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AwakeTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
